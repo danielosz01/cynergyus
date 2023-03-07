04752799fa94aa7bd6844b05c8ec3004390cb7ef
Daniel Benedeck <Daniel.Benedeck@cynergyus.com>
2023-03-07 08:15:25 -0800

    we add the modal function to our main sphere

--- cynergyus_react/src/App.tsx
+++ cynergyus_react/src/App.tsx
@@ -14,13 +14,14 @@ function App() {
   }
 
   return (
-    <div className="w-screen h-screen p-2 flex flex-col items-center overflow-hidden">
+    <div className="w-screen p-2 flex flex-col items-center">
       <h1 className='text-center font-bold text-2xl m-2'>React Photo Sphere</h1>
       
+      
       <PhotoSphere ></PhotoSphere>
-      <Spacing />
-      <TimeLine />
-      <Spacing />
+      
+      
+
       
 
     </div>
